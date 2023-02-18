286e732cd38a42ce420ea5ef9ed5c4aba82a0438
Anastasya <shamr44@mail.ru>
2023-02-18 03:29:46 +0300

    create layout and styles for sections

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__wrapper').slick({
+        adaptiveHeight: false,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png" alt="arrow"></button>',
+    });
+});
+
