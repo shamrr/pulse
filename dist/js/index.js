8924a3aa5a13729cb96b75f23a79089f91b4e695
Anastasya <shamr44@mail.ru>
2023-03-21 23:31:40 +0300

    change way to img

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__wrapper').slick({
         adaptiveHeight: false,
         cssEase: 'linear',
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png" alt="arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png" alt="arrow"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png" alt="arrow"></button>',
     });
 
     $('[data-modal=consultation]').on(('click'), function(){
